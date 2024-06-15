e6a470dca361fd58210eff5c28b9cbe3eef6d498
HP <HP@DESKTOP-PFGCHPS>
2024-06-15 15:08:31 +0530

    commit message5

---
+++ javacore/src/com/tns/programs/program5.java
@@ -0,0 +1,18 @@
+package com.tns.programs;
+import
+
+com.tns.programs.program5;
+
+public class program5 {
+
+	public static void main(String[] args) {
+		program3   u=new
+		program3();
+		
+		System.out.println(u.a);
+		System.out.println(u.f);
+
+
+	}
+
+}
