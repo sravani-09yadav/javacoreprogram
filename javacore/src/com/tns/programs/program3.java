2130b8a46457204f021cd961735bff15ad0cf402
HP <HP@DESKTOP-PFGCHPS>
2024-06-15 14:48:21 +0530

    commit message3

---
+++ javacore/src/com/tns/programs/program3.java
@@ -0,0 +1,21 @@
+package com.tns.programs;
+
+public class program3 {
+	int a=9;
+	protected char f='y';
+	public static void main(String[] args) {
+		program3   u=new
+				program3();
+		
+				System.out.println(u.a);
+					System.out.println(u.f);
+				
+					
+	}			
+					
+					
+					
+
+
+
+}
