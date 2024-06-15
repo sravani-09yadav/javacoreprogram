0dee0e6883a93defea0b2c0cafdc58807561d9a3
HP <HP@DESKTOP-PFGCHPS>
2024-06-15 14:53:07 +0530

    commite message4

---
+++ javacore/src/com/tns/programs/Fourthprogram.java
@@ -0,0 +1,13 @@
+package com.tns.programs;
+
+public class Fourthprogram {
+	public static void main(String[] args) {
+		program3   u=new
+				program3();
+		
+				System.out.println(u.a);
+					System.out.println(u.f);
+	}	
+					
+
+}
