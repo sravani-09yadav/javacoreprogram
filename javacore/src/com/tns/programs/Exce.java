ddf7f9392b9756a3643f28a78d6d85a40ec241bc
HP <HP@DESKTOP-PFGCHPS>
2024-06-18 09:21:22 +0530

    exce

---
+++ javacore/src/com/tns/programs/Exce.java
@@ -0,0 +1,19 @@
+package com.tns.programs;
+
+public class Exce {
+
+	public static void main(String[] args) {
+		
+		Pencapsulation              e=new
+				
+		Pencapsulation();
+		
+		 e.serialnumber=1;
+		 e.name="sravani";
+		 e.age=19;
+		 
+		 e.show();
+
+	}
+
+}
