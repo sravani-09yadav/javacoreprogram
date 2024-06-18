ac649633dfb1f16a0d077d00ec1c5de49ee5905f
HP <HP@DESKTOP-PFGCHPS>
2024-06-18 09:11:28 +0530

    commit message pencapsulation

---
+++ javacore/src/com/tns/programs/Pencapsulation.java
@@ -0,0 +1,21 @@
+package com.tns.programs;
+
+public class Pencapsulation  {
+	
+	     
+	int serialnumber;         //properties or data members
+	
+	String name;
+	
+	int age;
+	
+	public void show() {      //method or member function
+		System.out.println("serialnumber"+serialnumber+"  "  +"name"+name+"  "+"age "+age);
+		
+
+	}
+		
+	}
+	
+	
+
