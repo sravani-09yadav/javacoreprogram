f49c7854d9d27768189e79a95189e5bc88b88284
HP <HP@DESKTOP-PFGCHPS>
2024-07-05 20:37:11 +0530

    scanner class

---
+++ javacore/src/com/tns/scannerclass/ScannerExample.java
@@ -0,0 +1,24 @@
+package com.tns.scannerclass;
+
+ import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		     Scanner Sc=new   Scanner(System.in);
+		     
+		  System.out.println("enter your name");
+		      String name=Sc.nextLine();
+		      
+		      System.out.println("enter the student id");
+		      int  sid=Sc.nextInt();  
+		      
+		   System.out.println("enter the percentage");
+		      float   per=Sc.nextFloat();  
+		      
+	}
+		      
+		     
+}
+     
+
